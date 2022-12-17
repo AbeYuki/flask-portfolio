a7fdb941ce8d750a2ca0b05c3f5797f1a428784e
AbeYuki <aim4highergg@gmail.com>
2022-12-17 15:14:18 +0000

    coding

--- portfolio/app.py
+++ portfolio/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, Blueprint, render_template, redirect, url_for, request
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='./templates/images')
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
