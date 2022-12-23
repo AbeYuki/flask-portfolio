1b29568dad0393c139bcf4ac2ac5a2a43240cb13
AbeYuki <aim4highergg@gmail.com>
2022-12-23 05:42:27 +0000

    conding

--- portfolio/app.py
+++ portfolio/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, Blueprint, render_template, redirect, url_for, request
 
 def create_app():
-    app = Flask(__name__, static_folder='./templates/images')
+    app = Flask(__name__, static_folder='./template/images')
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- portfolio/main.py
+++ portfolio/main.py
@@ -2,14 +2,14 @@ from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
-@main.route('/portfolio')
+@main.route('/')
 def index():
     return render_template('index.html')
 
-@main.route('/portfolio/infrastructure')
+@main.route('/infrastructure')
 def infrastructure():
     return render_template('infrastructure.html')
 
-@main.route('/portfolio/software')
+@main.route('/software')
 def software():
     return render_template('software.html')
