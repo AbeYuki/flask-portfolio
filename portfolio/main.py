9e973dbc619c3c07773c2170ddd29e99978d8917
AbeYuki <aim4highergg@gmail.com>
2022-12-23 05:17:44 +0000

    coding

--- portfolio/main.py
+++ portfolio/main.py
@@ -2,14 +2,14 @@ from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
-@main.route('/')
+@main.route('/portfolio')
 def index():
     return render_template('index.html')
 
-@main.route('/infrastructure')
+@main.route('/portfolio/infrastructure')
 def infrastructure():
     return render_template('infrastructure.html')
 
-@main.route('/software')
+@main.route('/portfolio/software')
 def software():
     return render_template('software.html')
